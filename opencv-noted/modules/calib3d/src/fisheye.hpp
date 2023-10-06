a896b6f5eebc19acd1b4e09c1bb03156a2d9b7ee
blue-stone-w <junpengwang8@gmail.com>
2023-10-06 14:48:05 +0800

    update0002-reformat

--- opencv-noted/modules/calib3d/src/fisheye.hpp
+++ opencv-noted/modules/calib3d/src/fisheye.hpp
@@ -2,60 +2,64 @@
 #define FISHEYE_INTERNAL_H
 #include "precomp.hpp"
 
-namespace cv { namespace internal {
-
-struct CV_EXPORTS IntrinsicParams
+namespace cv
 {
-    Vec2d f;
-    Vec2d c;
-    Vec4d k;
-    double alpha;
-    std::vector<uchar> isEstimate;
+  namespace internal
+  {
 
-    IntrinsicParams();
-    IntrinsicParams(Vec2d f, Vec2d c, Vec4d k, double alpha = 0);
-    IntrinsicParams operator+(const Mat& a);
-    IntrinsicParams& operator =(const Mat& a);
-    void Init(const cv::Vec2d& f, const cv::Vec2d& c, const cv::Vec4d& k = Vec4d(0,0,0,0), const double& alpha = 0);
-};
+    struct CV_EXPORTS IntrinsicParams
+    {
+      Vec2d f; // 焦距
+      Vec2d c; // 中心
+      Vec4d k; // 畸变
+      double alpha;
+      std::vector<uchar> isEstimate;
 
-void projectPoints(cv::InputArray objectPoints, cv::OutputArray imagePoints,
-                   cv::InputArray _rvec,cv::InputArray _tvec,
-                   const IntrinsicParams& param, cv::OutputArray jacobian);
+      IntrinsicParams();
+      IntrinsicParams(Vec2d f, Vec2d c, Vec4d k, double alpha = 0);
+      IntrinsicParams operator+(const Mat &a);
+      IntrinsicParams &operator=(const Mat &a);
+      void Init(const cv::Vec2d &f, const cv::Vec2d &c, const cv::Vec4d &k = Vec4d(0, 0, 0, 0), const double &alpha = 0);
+    };
 
-void ComputeExtrinsicRefine(const Mat& imagePoints, const Mat& objectPoints, Mat& rvec,
-                            Mat&  tvec, Mat& J, const int MaxIter,
-                            const IntrinsicParams& param, const double thresh_cond);
-CV_EXPORTS Mat ComputeHomography(Mat m, Mat M);
+    void projectPoints(cv::InputArray objectPoints, cv::OutputArray imagePoints,
+                       cv::InputArray _rvec, cv::InputArray _tvec,
+                       const IntrinsicParams &param, cv::OutputArray jacobian);
 
-CV_EXPORTS Mat NormalizePixels(const Mat& imagePoints, const IntrinsicParams& param);
+    void ComputeExtrinsicRefine(const Mat &imagePoints, const Mat &objectPoints, Mat &rvec,
+                                Mat &tvec, Mat &J, const int MaxIter,
+                                const IntrinsicParams &param, const double thresh_cond);
+    CV_EXPORTS Mat ComputeHomography(Mat m, Mat M);
 
-void InitExtrinsics(const Mat& _imagePoints, const Mat& _objectPoints, const IntrinsicParams& param, Mat& omckk, Mat& Tckk);
+    CV_EXPORTS Mat NormalizePixels(const Mat &imagePoints, const IntrinsicParams &param);
 
-void CalibrateExtrinsics(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
-                         const IntrinsicParams& param, const int check_cond,
-                         const double thresh_cond, InputOutputArray omc, InputOutputArray Tc);
+    void InitExtrinsics(const Mat &_imagePoints, const Mat &_objectPoints, const IntrinsicParams &param, Mat &omckk, Mat &Tckk);
 
-void ComputeJacobians(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
-                      const IntrinsicParams& param,  InputArray omc, InputArray Tc,
-                      const int& check_cond, const double& thresh_cond, Mat& JJ2_inv, Mat& ex3);
+    void CalibrateExtrinsics(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
+                             const IntrinsicParams &param, const int check_cond,
+                             const double thresh_cond, InputOutputArray omc, InputOutputArray Tc);
 
-CV_EXPORTS void  EstimateUncertainties(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
-                           const IntrinsicParams& params, InputArray omc, InputArray Tc,
-                           IntrinsicParams& errors, Vec2d& std_err, double thresh_cond, int check_cond, double& rms);
+    void ComputeJacobians(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
+                          const IntrinsicParams &param, InputArray omc, InputArray Tc,
+                          const int &check_cond, const double &thresh_cond, Mat &JJ2_inv, Mat &ex3);
 
-void dAB(cv::InputArray A, InputArray B, OutputArray dABdA, OutputArray dABdB);
+    CV_EXPORTS void EstimateUncertainties(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
+                                          const IntrinsicParams &params, InputArray omc, InputArray Tc,
+                                          IntrinsicParams &errors, Vec2d &std_err, double thresh_cond, int check_cond, double &rms);
 
-void JRodriguesMatlab(const Mat& src, Mat& dst);
+    void dAB(cv::InputArray A, InputArray B, OutputArray dABdA, OutputArray dABdB);
 
-void compose_motion(InputArray _om1, InputArray _T1, InputArray _om2, InputArray _T2,
-                    Mat& om3, Mat& T3, Mat& dom3dom1, Mat& dom3dT1, Mat& dom3dom2,
-                    Mat& dom3dT2, Mat& dT3dom1, Mat& dT3dT1, Mat& dT3dom2, Mat& dT3dT2);
+    void JRodriguesMatlab(const Mat &src, Mat &dst);
 
-double median(const Mat& row);
+    void compose_motion(InputArray _om1, InputArray _T1, InputArray _om2, InputArray _T2,
+                        Mat &om3, Mat &T3, Mat &dom3dom1, Mat &dom3dT1, Mat &dom3dom2,
+                        Mat &dom3dT2, Mat &dT3dom1, Mat &dT3dT1, Mat &dT3dom2, Mat &dT3dT2);
 
-Vec3d median3d(InputArray m);
+    double median(const Mat &row);
 
-}}
+    Vec3d median3d(InputArray m);
+
+  }
+}
 
 #endif
